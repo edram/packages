bae6b9707a246cbc42c1b8a1aef69677917ea402
edram <edram@qq.com>
2024-02-20 08:30:51 +0800

    test(react-hooks): 增加 navigator undefined 的情况

--- packages/react-hooks/tests/useUAParser.test.ts
+++ packages/react-hooks/tests/useUAParser.test.ts
@@ -5,7 +5,7 @@ describe('useUAParser', () => {
   it('解析 ua', () => {
     const ua =
       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36';
-    vi.spyOn(global.navigator, 'userAgent', 'get').mockReturnValue(ua);
+    vi.spyOn(globalThis.navigator, 'userAgent', 'get').mockReturnValue(ua);
 
     const { result } = renderHook(() => {
       return useUAParser();
@@ -22,5 +22,16 @@ describe('useUAParser', () => {
       version: '121.0.0.0',
     });
     expect(current.getUA()).toEqual(ua);
+  });
+
+  it('测试 navigator undefined 的情况', () => {
+    // @ts-expect-error nextjs 中 navigator 为 undefined
+    vi.spyOn(globalThis, 'navigator', 'get').mockReturnValue(undefined);
+
+    const { result } = renderHook(() => {
+      return useUAParser();
+    });
+
+    expect(result.error).toBeUndefined();
   });
 });
